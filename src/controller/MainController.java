e948833e6b09576026528d0484388eda702180f9
Ho Ngoc Hieu <youremail@gmail.com>
2025-10-23 20:05:13 +0700

    hieu up

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,5 +1,11 @@
 package controller;
 
 public class MainController {
+<<<<<<< HEAD
 
+=======
+    public static void main(String[] args) {
+
+    }
+>>>>>>> c1c908df6112e7d567626358b6712ce541aa50a3
 }
